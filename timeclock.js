2537c56b986247dca812cbaa0cc41b6156d4ab57
mrsantacruz86 <abnerdc86@gmail.com>
2018-03-12 18:49:24 -0400

    updating

--- timeclock.js
+++ timeclock.js
@@ -1,10 +1,10 @@
 var config = {
-  apiKey: "AIzaSyCor3jn1XIEVPGnWvF76usBd7hdxY1_QG4",
-  authDomain: "time-clock-eecd3.firebaseapp.com",
-  databaseURL: "https://time-clock-eecd3.firebaseio.com",
-  projectId: "time-clock-eecd3",
-  storageBucket: "time-clock-eecd3.appspot.com",
-  messagingSenderId: "444204466941"
+  apiKey: "AIzaSyCpbiT0CCGENNNqZ7CLlN3bdY-7lkyjlu0",
+  authDomain: "time-tracking-app-835f2.firebaseapp.com",
+  databaseURL: "https://time-tracking-app-835f2.firebaseio.com",
+  projectId: "time-tracking-app-835f2",
+  storageBucket: "time-tracking-app-835f2.appspot.com",
+  messagingSenderId: "10151088161"
 };
 firebase.initializeApp(config);
 
@@ -19,16 +19,39 @@ function createNewEmployee(name, role, startDate, monthlyRate) {
     monthlyRate: monthlyRate,
   };
     
-  database.ref().push(employee);
+  database.ref('people').push(employee);
 }
 
+function addRecordToTable(key, name, role, startDate, rate) {
+  var $record = $('tr');
+  var $cell = $('td');
+  $record.attr('key', key);
+  $cell.html(name).appendTo($record);
+  $cell.html(role).appendTo($record);
+  $cell.html(startDate).appendTo($record);
+  $cell.html(rate).appendTo($record);
+  $record.appendTo()
+}
+$(document).ready(function(){
 
-$("#submitBtn").click(function (event) {
-  event.preventDefault();
 
-  var empName = $('#emName').val();
-  var startD = $('#startDate').val();
-  var role = $('#role').val();
-  var rate = $('#monthlyRate').val();
-  createNewEmployee(empName, startD, role, rate);
-})
+  $("#submitBtn").click(function (event) {
+    event.preventDefault();
+    var empName = $('#emName').val();
+    var startD = $('#startDate').val();
+    var role = $('#role').val();
+    var rate = $('#monthlyRate').val();
+    $('#dataEntry input').val("");
+    createNewEmployee(empName, startD, role, rate);
+  })
+});
+  
+var ref = database.ref('people');
+ref.on("child_added", function (snapshot) {
+  var k = snapshot.key;
+  
+  console.log(k + snapshot.val().name);
+  
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
